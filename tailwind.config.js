8ce60cc2095cdcf64fb9996ee775668be0f02a2a
xXxBiraxXx <115120177+xXxBiraxXx@users.noreply.github.com>
2023-02-07 22:29:16 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    boxShadow: {
+      'DEFAULT': ' rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;',
+      'sm': ' rgba(100, 100, 111, 0.2) 0px 4px 16px 0px;',
+      'xs': ' rgba(100, 100, 111, 0.2) 0px 2px 8px 0px;',
+    }
+  },
+  plugins: [],
+}
